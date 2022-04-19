0a001d003055e78345881b9033c58bc99269cc05
Erik Fleckstein <erik@tinkerforge.com>
2022-04-19 16:04:30 +0200

    examples: uc: Rename TF_HalContext to TF_HAL.

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -9,19 +9,19 @@
 
 void check(int rc, const char* msg);
 
-void example_setup(TF_HalContext *hal);
-void example_loop(TF_HalContext *hal);
+void example_setup(TF_HAL *hal);
+void example_loop(TF_HAL *hal);
 
 
 static TF_SolidStateRelayV2 ssr;
 
-void example_setup(TF_HalContext *hal) {
+void example_setup(TF_HAL *hal) {
 	// Create device object
 	check(tf_solid_state_relay_v2_create(&ssr, UID, hal), "create device object");
 
 	// Turn relay on/off 10 times with 1 second delay
 	int i;
-	for(i = 0; i < 5; ++i) {
+	for (i = 0; i < 5; ++i) {
 		tf_hal_sleep_us(hal, 1000 * 1000);
 		check(tf_solid_state_relay_v2_set_state(&ssr, true), "call set_state");
 		tf_hal_sleep_us(hal, 1000 * 1000);
@@ -29,7 +29,7 @@ void example_setup(TF_HalContext *hal) {
 	}
 }
 
-void example_loop(TF_HalContext *hal) {
+void example_loop(TF_HAL *hal) {
 	// Poll for callbacks
 	tf_hal_callback_tick(hal, 0);
 }
